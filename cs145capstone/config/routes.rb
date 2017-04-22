252aefd688abb0f3f6c7e032a4eaff19e6fee28f
Aimee <gaimeenicole@gmail.com>
2017-04-22 21:18:10 +0800

    Upload to AWS working

--- cs145capstone/config/routes.rb
+++ cs145capstone/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'uploads/new'
+
+  get 'uploads/create'
+
+  get 'uploads/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :uploads
+  root 'uploads#new'
 end
